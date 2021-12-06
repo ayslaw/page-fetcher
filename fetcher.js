11678083ba2fb990b7d6b8e1f9791d144d3e5fe2
ayslaw <ayoubf.chp@gmail.com>
2021-12-06 03:50:25 -0500

    code to write the HTML

---
+++ fetcher.js
@@ -0,0 +1,21 @@
+const request = require('request'); // for HTTP request
+const fs = require('fs') // for writing to file
+const args = process.argv.slice(2); // takes cmd line arguments
+
+let urlArg = args[0];
+let pathArg = args[1];
+
+
+// HTTP request
+request(urlArg, (error, response, body) => {
+  // Write to file
+  fs.writeFile(pathArg, body, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    // Confirmation message
+    const {size} = fs.statSync(pathArg);
+    console.log(`Downloaded and saved ${size} bytes to ./index.html`)
+  })
+});
